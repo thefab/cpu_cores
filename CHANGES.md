9ab16162f71abd609b8a7d18fdb9fedadb292c26
Fabien MARTY <fabien.marty@gmail.com>
2013-08-31 13:39:32 +0200

    new CHANGES.md file and fix PyPi installation problems

---
+++ CHANGES.md
@@ -0,0 +1,14 @@
+# CHANGES
+
+## Release 0.1.2 (alpha)
+
+- another release on PyPi to be coherent with git tags
+
+## Release 0.1.1 (alpha)
+
+- fix an installation problem with PyPi
+- add a CHANGES.md file
+
+## Release 0.1.0 (alpha)
+
+- first release
--- cpu_cores/__init__.py
+++ cpu_cores/__init__.py
@@ -1,7 +1,7 @@
 # This file is part of cpu_cores released under the MIT license.
 # See the LICENSE file for more information.
 
-version_info = (0, 1, '0')
+version_info = (0, 1, '1')
 __version__ = ".".join([str(x) for x in version_info])
 
 from cpu_cores.common import CPUCoresCounter
